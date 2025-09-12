b35f0815a8a4ec6e88b0ab9a4553d57556194097
Adilson Cuevas <cuevasadilson@gmail.com>
2025-09-12 16:09:00 -0500

    cargar cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.CORS_ORIGIN ?? 'https://frontend-angular-mu-nine.vercel.app',
-    methods: 'GET,,PUT,POST,DELETE',
+    methods: 'GET,PUT,POST,DELETE,OPTIONS',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 4000);
