60b99b5a4b0397a9c7a285524362730bac5bc3a2
Adilson Cuevas <cuevasadilson@gmail.com>
2025-09-14 11:14:52 -0500

    pruebas sin cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import { Request, Response } from 'express';
 import * as express from 'express';
+import { Request, Response } from 'express';
 
 let cachedHandler: any;
 
@@ -10,18 +10,15 @@ async function bootstrapServer() {
   if (!cachedHandler) {
     const expressApp = express();
 
-    expressApp.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'https://frontend-angular-liard.vercel.app');
-      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      // si no usas cookies, no pongas Access-Control-Allow-Credentials
-      if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-      }
-      next();
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      allowedHeaders: 'Content-Type, Authorization',
+      credentials: false,
     });
 
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
     await app.init();
 
     cachedHandler = expressApp;
