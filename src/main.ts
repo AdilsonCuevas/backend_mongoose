74abc378a59300db04db3c6b549783bac17f3980
Adilson Cuevas <cuevasadilson@gmail.com>
2025-09-13 20:08:51 -0500

    abrir los cros

--- src/main.ts
+++ src/main.ts
@@ -5,28 +5,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // Middleware global CORS
-  app.use((req, res, next) => {
-    res.setHeader(
-      'Access-Control-Allow-Origin',
-      'https://frontend-angular-liard.vercel.app'
-    );
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
-    );
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization'
-    );
-
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-    next();
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN ?? 'https://frontend-angular-liard.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT ?? 4000);
