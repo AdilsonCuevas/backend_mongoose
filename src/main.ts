7cedc2bfed92857a9a91cd16f2cb4eaac621ed4a
Adilson Cuevas <cuevasadilson@gmail.com>
2025-09-14 11:33:12 -0500

    actualizacion de express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 
 let cachedHandler: any;
 
@@ -13,8 +12,9 @@ async function bootstrapServer() {
     const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
 
     app.enableCors({
-      origin: [process.env.CORS_ORIGIN ?? 'https:localhost:4200', 'https://frontend-angular-liard.vercel.app/'],
-      credentials: false,
+      origin: process.env.CORS_ORIGIN ?? 'https://frontend-angular-liard.vercel.app/',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      allowedHeaders: 'Content-Type, Authorization',
     });
 
     await app.init();
@@ -28,3 +28,6 @@ export default async function handler(req: Request, res: Response) {
   const server = await bootstrapServer();
   server(req, res);
 }
+
+
+
