0928c4ab19406f262712fe7b8417ca4d5c4f31b5
Adilson Cuevas <cuevasadilson@gmail.com>
2025-09-13 19:55:01 -0500

    actualizando los cors

--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,25 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'https://frontend-angular-liard.vercel.app',
-      process.env.CORS_ORIGIN,
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
 
-  // Middleware manual para asegurar preflight en Render
+  // Middleware global CORS
   app.use((req, res, next) => {
-    res.header(
+    res.setHeader(
       'Access-Control-Allow-Origin',
-      'https://frontend-angular-liard.vercel.app',
+      'https://frontend-angular-liard.vercel.app'
     );
-    res.header(
+    res.setHeader(
       'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
     );
-    res.header(
+    res.setHeader(
       'Access-Control-Allow-Headers',
-      'Content-Type, Authorization',
+      'Content-Type, Authorization'
     );
 
     if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);
+      res.status(200).end();
+      return;
     }
     next();
   });
