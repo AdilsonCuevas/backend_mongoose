caf03cd22c15c49bce31bb150ae3aab4f7139827
Adilson Cuevas <cuevasadilson@gmail.com>
2025-09-13 19:35:16 -0500

    agegar middleware para cors

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,28 @@ async function bootstrap() {
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
+  // Middleware manual para asegurar preflight en Render
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://frontend-angular-liard.vercel.app',
+    );
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    );
+
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
